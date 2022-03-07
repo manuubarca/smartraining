7b3f4281eeda89bbe789400550c0891893ed9a0b
Manuel Barca <mbarcaburguez1@gmail.com>
2022-03-07 14:27:02 -0300

    pagina planes

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
 //hago formulario en js vinculado con los imputs del html
 let formSocio = document.getElementById('formSocio');
-let botonRegistrarse = document.getElementById('botonEnviarFormularioNuevoSocio');
+let botonRegistrarse = document.getElementById('botonRegistrarse');
 let divSocios = document.getElementById('divSocios');
 
 //Agregando eventos a elementos del DOM
@@ -10,6 +10,7 @@ formSocio.addEventListener('submit', (e) => {
     let nombreImput = document.getElementById('idNombre').value
     let apellidoImput = document.getElementById('idApellido').value
     let fechaDeNacimientoImput = document.getElementById('idFechaDeNacimiento').value
+    let sexoImput = document.getElementById('idSexo').value
     let ciudadImput = document.getElementById('idCiudad').value
     let direccionImput = document.getElementById('idDireccion').value
     let emailImput = document.getElementById('idEmail').value
@@ -21,6 +22,7 @@ formSocio.addEventListener('submit', (e) => {
         nombreSocio: nombreImput,
         apellidoSocio: apellidoImput,
         fechaDeNacimientoSocio: fechaDeNacimientoImput,
+        sexoSocio: sexoImput,
         ciudadSocio: ciudadImput,
         direccionSocio: direccionImput,
         emailSocio: emailImput,
@@ -39,64 +41,52 @@ botonRegistrarse.addEventListener('clic', () => {
     divSocios.innerHTML = ""
     socios.forEach((Socio, indice) => {
         divSocios.innerHTML += `
-        <div class="container-fluid mb-5 text-start">
-            <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center ">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-body p-0">
-                                <div id="socio${indice} class="row g-0">
-                                    <h3 class="fw-normal mb-5 fw-bold">
-                                        Información personal
-                                    </h3>
-                                    <div class="row">
-                                        <div class="col-md-4 mb-4 pb-2">
-                                            <p>Nombre: ${Socio.nombreImput}
-                                        </div>
-                                        <div class="col-md-4 mb-4 pb-2">
-                                            <p>Apellido: ${Socio.apellidoImput}
-                                        </div>
-                                        <div class="col-md-4 mb-4 pb-2">
-                                            <input type="file" id="img" name="img" accept="image/*">
-                                        </div>
-                                    </div>
-                                    <div class="mb-4 pb-2">
-                                        <p>Apellido: ${Socio.fechaDeNacimientoImput}
-                                    </div>
-                                    <div class="row mb-4 pb-2">
-                                        <div class="col-md-6 mb-4 pb-2 mb-md-0 pb-md-0">
-                                            <p>Ciudad: ${Socio.ciudadImput}
-                                        </div>
-                                        <div class="col-md-6">
-                                            <p>Dirección: ${Socio.direccionImput}
-                                        </div>
-                                    </div>
-                                    <div class="row mb-4 pb-2">
-                                        <div class="col-md-6 mb-4 pb-2 mb-md-0 pb-md-0">
-                                            <p>Email: ${Socio.emailImput}
-                                        </div>
-                                        <div class="col-md-6">
-                                            <p>Apellido: ${Socio.contraseniaImput}
-                                        </div>
-                                    </div>
-                                    <div class="mb-4 pb-2">
-                                        <p>Número de celular: ${Socio.numeroDeCelularImput}
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 mb-4 pb-2 mb-md-0 pb-md-0">
-                                            <p>Apellido: ${Socio.prestadorDeSaludImput}
-                                        </div>
-                                        <div class="col-md-6">
-                                            <p>Apellido: ${Socio.carneDeSaludImput}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <div id="socio${indice} class="row g-0">
+                <h3 class="fw-normal mb-5 fw-bold">
+                    Información personal
+                </h3>
+                <div class="row">
+                    <div class="col-md-4 mb-4 pb-2">
+                        <p>Nombre: ${Socio.nombreImput}
+                    </div>
+                    <div class="col-md-4 mb-4 pb-2">
+                        <p>Apellido: ${Socio.apellidoImput}
+                    </div>
+                    <div class="col-md-4 mb-4 pb-2">
+                        <input type="file" id="img" name="img" accept="image/*">
+                    </div>
+                </div>
+                <div class="mb-4 pb-2">
+                    <p>Apellido: ${Socio.fechaDeNacimientoImput}
+                </div>
+                <div class="row mb-4 pb-2">
+                    <div class="col-md-6 mb-4 pb-2 mb-md-0 pb-md-0">
+                        <p>Ciudad: ${Socio.ciudadImput}
+                    </div>
+                    <div class="col-md-6">
+                        <p>Dirección: ${Socio.direccionImput}
+                    </div>
+                </div>
+                <div class="row mb-4 pb-2">
+                    <div class="col-md-6 mb-4 pb-2 mb-md-0 pb-md-0">
+                        <p>Email: ${Socio.emailImput}
+                    </div>
+                    <div class="col-md-6">
+                        <p>Apellido: ${Socio.contraseniaImput}
+                    </div>
+                </div>
+                <div class="mb-4 pb-2">
+                    <p>Número de celular: ${Socio.numeroDeCelularImput}
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-4 pb-2 mb-md-0 pb-md-0">
+                        <p>Apellido: ${Socio.prestadorDeSaludImput}
+                    </div>
+                    <div class="col-md-6">
+                        <p>Apellido: ${Socio.carneDeSaludImput}
                     </div>
                 </div>
             </div>
-        </div>
         `
     });
 })
